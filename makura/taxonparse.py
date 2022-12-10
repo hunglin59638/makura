1e067ab3d8d5158dc682b5a54d7d2c9607ef3bf7
Hung-Lin, Chen <hunglin59638@gmail.com>
2022-12-10 17:09:19 +0800

    MAN-2 module for parsing taxonomy

---
+++ makura/taxonparse.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+from pathlib import Path
+from ete3 import NCBITaxa
+
+TAXDUMP_FILE = None
+
+
+class TaxonomyParser(NCBITaxa):
+    def __init__(self, dbfile=None, taxdump_file=None):
+        super().__init__(dbfile, taxdump_file)
+
+    def convert_name_to_taxid(self, names):
+        pass
+
+    def get_lineage_translator(self, taxids):
+        return super().get_lineage_translator(taxids)
